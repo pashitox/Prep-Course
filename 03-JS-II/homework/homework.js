cf4bb62f1c04fed369c1623c88e00a2c6d886f49
Ing.Juan Gonzalez <pashitox@gmail.com>
2022-04-27 17:48:21 +0200

    comienos de ejecicios 3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,10 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+
+
+
+  
 }
 
 function mayoriaDeEdad(edad) {
