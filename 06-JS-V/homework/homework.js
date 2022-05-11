e589af24993e70872486e2c78e06f211077d8556
Ing.Juan Gonzalez <pashitox@gmail.com>
2022-05-11 13:11:51 +0200

    ecv

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -71,11 +71,11 @@ function agregarStringInvertida() {
   String.prototype.reverse = function() {
     var stringInvertida = '';
 
-    console.log(this.length);
+   
     for(var i = this.length - 1; i>=0; i--) {
       stringInvertida = stringInvertida + this.charAt(i);
 
-      console.log(this.charAt(i));
+      
     }
     return stringInvertida;
   };
@@ -107,6 +107,9 @@ function agregarStringInvertida() {
          this.apellido = apellido,
          this.edad = edad, 
          this.domicilio= domicilio
+
+        console.log(this.detalle);
+
          this.detalle = function(){
          
           return { nombre: this.nombre,
@@ -129,11 +132,30 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+
+         nombre = this.nombre,
+         this.apellido = apellido,
+         this.edad = edad, 
+         this.dir= dir
+
+   var persona = new Persona("Juan", "Perez", 22, "Saavedra 123")
+
+   return persona;
+
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+
+
+  Persona.prototype.datos = function() {
+    return `${this.nombre}, ${this.edad} años`;
+  };
+
+
+
+
 }
   
 
